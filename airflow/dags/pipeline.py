0c2c69ce771b939a6bd7fd351ad93aae1bbc5df1
macedoraf <macedo.rafanazareth@gmail.com>
2025-10-25 18:48:10 -0300

    fix: starting date

--- airflow/dags/pipeline.py
+++ airflow/dags/pipeline.py
@@ -25,7 +25,7 @@ AIRFLOW_TRANSFORMED_DATA_PATH = '/opt/airflow/data/transformed_data.csv'
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 1, 1),
+    'start_date': datetime(2025, 10, 25),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
